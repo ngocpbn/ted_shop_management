cadc03fb546a8e56d6d6fbfb9110fc11a7bda7a8
NgocPB <bichngoc.phamgg@gmail.com>
2023-12-22 20:13:54 +0700

    add model

---
+++ Windows Form Final - Tedshop System/BusinessObjects/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Form_Final___Tedshop_System.BusinessObjects
+{
+    internal class Product
+    {
+        public int ProductID {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int SupplierID { get; set; }
+    }
+}
