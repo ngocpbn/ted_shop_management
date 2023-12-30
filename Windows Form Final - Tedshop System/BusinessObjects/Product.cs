dd8f082936aef059f3510f14c68332cf8e0ed4e0
Tiendao <tiendq@perfin.io>
2023-12-30 20:00:43 +0700

    resolve conflic

--- Windows Form Final - Tedshop System/BusinessObjects/Product.cs
+++ Windows Form Final - Tedshop System/BusinessObjects/Product.cs
@@ -10,16 +10,22 @@ namespace Windows_Form_Final___Tedshop_System.BusinessObjects
     public class Product
     {
         [Key]
-        public int Product_ID {  get; set; }
+        public int Product_ID { get; set; }
 
 
         [StringLength(20)]
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
 
         [StringLength(30)]
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public int Supplier_ID { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; } = 0.0m;
+        public int Stock { get; set; } = 0;
+        public int Supplier_ID { get; set; } = 0;
+
+        [StringLength(10)]
+        public string Size { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string Category { get; set; } = string.Empty;
     }
 }
